1386315617d115a58c81ec83220c78e4845b6ca6
John Cen <john.cen397@gmail.com>
2019-10-21 21:23:48 -0400

    initial setup

---
+++ xintern-ui/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>xintern-ui-playground</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
