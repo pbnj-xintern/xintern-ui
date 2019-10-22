c50ff60c670141c53d4dd9274b03b2d21f49efd0
John Cen <john.cen397@gmail.com>
2019-10-22 01:50:56 -0400

    review-list-card template near done, some responsive debugging needed

--- xintern-ui/src/components/review-list-card/index.js
+++ xintern-ui/src/components/review-list-card/index.js
@@ -1,10 +1,53 @@
+/** @jsx jsx */ import { jsx } from '@emotion/core'
 import React, { useState } from 'react'
 import * as styles from './review-list-card.emotion'
-
+import Logo from '../../logo192.png'
 
 const ReviewListCard = () => {
+    const [companyLogo, setCompanyLogo] = useState("")
+    const [reviewTitle, setReviewTitle] = useState("Review Title")
+    const [companyName, setCompanyName] = useState("Company Name")
+    const [username, setUsername] = useState("username")
+
+    const [overallRating, setOverallRating] = useState("4.5")
+    const [upvotes, setUpvotes] = useState([])
+    const [downvotes, setDownvotes] = useState([])
+    const [comments, setComments] = useState([])
+
     return (
-        <h1>Hello this is a card.</h1>
+        <div css={styles.CardContainer}>
+            <div css={styles.CompanyLogoContainer}>
+                <img src={Logo} style={{ objectFit: 'contain', width: '70%' }} alt="company_logo" />
+            </div>
+            <div css={styles.ContentContainer}>
+                <div css={styles.ReviewInfoContainer}>
+                    <h4 css={styles.ReviewText}>{reviewTitle}</h4>
+                    <h4 css={styles.ReviewText}>{companyName}</h4>
+                    <div css={styles.MetaDataContainer}>
+                        <h4 css={styles.ReviewText}>{username}</h4>
+                        <h4 css={styles.ReviewText}>date/time</h4>
+                    </div>
+                </div>
+                <div css={styles.ReviewRatingsContainer}>
+                    <div css={styles.RatingContainer}>
+                        <h3 css={styles.RatingValue}>{overallRating}</h3>
+                        <h6 css={styles.RatingLabel}>rating label</h6>
+                    </div>
+                    <div css={styles.RatingContainer}>
+                        <h3 css={styles.RatingValue}>123</h3>
+                        <h6 css={styles.RatingLabel}>upvotes</h6>
+                    </div>
+                    <div css={styles.RatingContainer}>
+                        <h3 css={styles.RatingValue}>12</h3>
+                        <h6 css={styles.RatingLabel}>downvotes</h6>
+                    </div>
+                    <div css={styles.RatingContainer}>
+                        <h3 css={styles.RatingValue}>4321</h3>
+                        <h6 css={styles.RatingLabel}>comments</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- xintern-ui/src/components/review-list-card/review-list-card.emotion.js
+++ xintern-ui/src/components/review-list-card/review-list-card.emotion.js
@@ -1,14 +1,110 @@
 import { css } from '@emotion/core'
 // import bp from '../../styles/breakpoint'
 
-// export const carousel = css`
-//    display: flex;
-//    flex-direction: row;
-//    overflow: hidden;
-//    width: 360px;
-//    height: 300px;
-//    position: absolute;
+export const CardContainer = css`
+    display: flex;
+    flex-direction: row;
+    overflow: hidden;
+    width: 950px;
+    height: 125px;
+    background-color: gray; //remove after
+    border-radius: 1.25em;
+    padding: 0.5em 1.25em;
+    cursor: pointer;
 
+    &:hover{
+        border: 0.5px lightgreen solid;
+    }
+`
+export const CompanyLogoContainer = css`
+    display: flex;
+    height: 100%;
+    width: 13.5%;
+    background-color: honeydew; //remove after
+    border-radius: 1.5em;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+`
+export const ContentContainer = css`
+    display: flex;
+    flex-direction: row;
+    width: 85%;
+    height: calc(100% - 0.5em);
+    // background-color: lightgreen; //remove after
+    border-radius: 1em;
+    // margin-left: 1.5%;
+    padding: 0.2em 0.5em;
+    align-items: center;
+    position: relative;
+`
+export const ReviewInfoContainer = css`
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    width: 35%; 
+    // background-color: purple; //remove after
+    border-radius: 1em;
+    justify-content: center;
+    position: absolute;
+    left: 15px;
+`
+export const ReviewRatingsContainer = css`
+    display: flex;
+    flex-direction: row;
+    height: calc(100% - 0.6em);
+    width: 65%; 
+    // background-color: teal; //remove after
+    border-radius: 1em;
+    justify-content: flex-end;
+    align-items: center;
+    // margin-left: 0.51em;
+    // margin-right: -1em;
+    position: absolute;
+    right: 0;
+`
+
+export const MetaDataContainer = css`
+    display: flex;
+    flex-direction: row;
+    width: 100%;   
+    justify-content: left;
+    align-items: center;
+`
+
+export const ReviewText = css`
+    font-weight: 100;
+    margin: 0.35em 1em;
+    text-align: left;
+    font-size: 20px;
+    width: 80%;
+    height: fit-content;
+    // position: absolute;
+`
+export const RatingContainer = css`
+    display: flex;
+    flex-direction: column;
+    background-color: cadetblue; //remove after
+    height: 100%;
+    width: 20%;
+    margin: 0em 0.25em;
+    border-radius: 1em;
+    algin-items: center;
+    justify-content: center;
+    cursor: pointer;
+`
+
+export const RatingValue = css`
+    font-size: 37px;
+    font-weight: 100;
+    margin: 0.2em 0em;
+`
+
+export const RatingLabel = css`
+    font-size: 15px;
+    font-weight: 300;
+    margin: 0em;
+`
 //     ${bp['medium']} {
 //         width: 720px;
 //     }
