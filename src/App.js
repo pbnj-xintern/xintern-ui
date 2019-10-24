174145d3a45a0914beaa2293bba3466849b1ed78
John Cen <john.cen397@gmail.com>
2019-10-24 19:10:52 -0400

    Added react-router; testing routing

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import Homepage from './layouts/homepage/homepage'
 import Navbar from './components/navbar/index'
 import 'antd/dist/antd.css';
-// import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom'
 import './App.css';
 import 'antd/dist/antd.css';
+import BrowseCompanies from './layouts/browse-companies/browse-companies';
 
 function App() {
   return (
     <div className="App" style={{ backgroundImage: `url('/images/bg.png')`, backgroundRepeat: 'repeat' }}>
       <Navbar search={false} />
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/companies" component={BrowseCompanies} />
+      </Switch>
     </div>
   );
 }
---
+++ src/layouts/browse-companies/browse-companies.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react'
+import { Row, Col } from 'antd'
+
+const BrowseCompanies = () => {
+    return (
+        <Row style={{ padding: "0em 17em" }}>
+            <Col>
+                <h1>this is where list of companies go</h1>
+            </Col>
+        </Row>
+    )
+}
+
+export default BrowseCompanies
