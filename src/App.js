601959fc50de13d46b3957e57bca83364bd63b53
Praiyon <praiyon@gmail.com>
2019-10-23 21:42:29 -0400

    searchbar and navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReviewListCard from './components/review-list-card/index'
+import Navbar from './components/navbar/index'
+import 'antd/dist/antd.css';
 // import logo from './logo.svg';
 import './App.css';
 
@@ -7,9 +9,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
+        <Navbar search={true} />
         <h1>xintern-ui</h1>
-        <ReviewListCard />
       </header>
     </div>
   );
