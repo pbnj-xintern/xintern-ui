44b50a2fca98238448e8267af3baed75bd30278d
John Cen <john.cen397@gmail.com>
2019-10-25 00:27:04 -0400

    temp fix to center carousel

--- src/components/small-company-card/small-company-card.emotion.js
+++ src/components/small-company-card/small-company-card.emotion.js
@@ -4,6 +4,7 @@ export const Card = css`
     border-radius:20px;
     width: 250px;
     margin-top: 5px;
+    margin-left: -13%;
     -webkit-box-shadow: 6px 10px 31px -17px rgba(0,0,0,0.56);
     -moz-box-shadow: 6px 10px 31px -17px rgba(0,0,0,0.56);
     box-shadow: 6px 10px 31px -17px rgba(0,0,0,0.56);
