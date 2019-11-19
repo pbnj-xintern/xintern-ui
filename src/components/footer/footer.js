70ec59c45c0f0e8c6fe27ac8dfeaff40595c919a
John Cen <john.cen397@gmail.com>
2019-11-18 22:54:01 -0500

    Introduces Footer component

---
+++ src/components/footer/footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Row, Col } from 'antd'
+import { useLocation } from 'react-router-dom'
+
+const Footer = () => {
+    let location = useLocation()
+    console.log("location pathname:", location.pathname)
+    return (
+        (location.pathname === "/login" || location.pathname === "/signup") ? null : 
+        (<Row style={{ height: "100px" }}>
+            <Col style={{ display: "flex", height: "100%", justifyContent: "center", alignItems: "center", backgroundColor: "rgb(21, 97, 173)" }}>
+                <h4 style={{ fontWeight: "200", color: "white" }}>© 2019 Copyright. xIntern</h4>
+            </Col>
+        </Row>)
+    )
+}
+
+export default Footer
