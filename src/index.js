45fee16d126cc27bc794e042713c4a2215dea49e
byan1197 <Bondyan97@gmail.com>
2019-11-21 16:55:31 -0500

    env path for api base url

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom'
 import axios from 'axios'
 
-const ENV_PATH = process.env.NODE_ENV === "development"? "dev/" : ""
+const ENV_PATH = process.env.REACT_APP_ENV_PATH
+
 axios.defaults.baseURL = "https://mmu5kk85li.execute-api.us-east-2.amazonaws.com/" + ENV_PATH
 
 axios.interceptors.request.use(function (config) {
@@ -16,8 +17,4 @@ axios.interceptors.request.use(function (config) {
 });
 
 ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
