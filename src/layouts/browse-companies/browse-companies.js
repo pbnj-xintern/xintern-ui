4422337ce85e88df469da4525742801a33ae2f80
byan1197 <Bondyan97@gmail.com>
2019-11-21 23:25:06 -0500

    completed functionality

--- src/layouts/browse-companies/browse-companies.js
+++ src/layouts/browse-companies/browse-companies.js
@@ -1,7 +1,9 @@
-import React, { useState, useEffect } from 'react'
-import { Row, Col, List, Icon } from 'antd'
-import axios from 'axios'
-import CompanyListCard from '../../components/company-list-card/company-list-card'
+import { Col, Icon, Input, List, Row } from 'antd';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import CompanyListCard from '../../components/company-list-card/company-list-card';
+const { Search } = Input;
+
 
 const getAllCompanies = async () => {
     try {
@@ -19,33 +21,142 @@ const getAllCompanies = async () => {
 const BrowseCompanies = () => {
     let [allCompanies, setAllCompanies] = useState([])
     let [isLoading, setLoading] = useState([])
+    let [filteredCompanies, setFilteredCompanies] = useState([])
+    let [typingTimer, setTypingTimer] = useState(null)
 
     useEffect(() => {
         setLoading(true)
         const fetchAllCompanies = async () => {
-            setAllCompanies(await getAllCompanies())
+            let allCompanies = await getAllCompanies()
+            setAllCompanies(allCompanies)
+            setFilteredCompanies(allCompanies)
             setLoading(false)
         }
         fetchAllCompanies()
     }, [])
 
-    const headerStyle = { fontWeight: "500", marginTop: '13%' }
+    const onSearch = valueFromSearchBar => {
+        clearTimeout(typingTimer);
+        let filterTerm = valueFromSearchBar.replace(/[\W_]+/g, '').toLowerCase()
+
+        setFilteredCompanies(
+            allCompanies.filter(c => c.name.replace(/[\W_]+/g, '').toLowerCase().includes(filterTerm))
+        )
+    }
+
+    const onInputChange = e => {
+        clearTimeout(typingTimer);
+        let filterTerm = e.target.value.replace(/[\W_]+/g, '').toLowerCase()
+
+        setTypingTimer(setTimeout(() => {
+            setFilteredCompanies(
+                allCompanies.filter(c => c.name.replace(/[\W_]+/g, '').toLowerCase().includes(filterTerm))
+            )
+        }, 750));
+    }
+
+    const headerStyle = { fontWeight: "500" }
+
+    const jumpToId = letter => {
+        let ele = document.getElementsByClassName('letter-' + letter.toLowerCase())
+        if (ele.length > 0) {
+            ele[0].scrollIntoView()
+        }
+    }
+
+    const getSidebarButtons = () => {
+        let buttonList = [];
+        buttonList.push(<li
+            id='rolodex'
+            onClick={() => jumpToId('#')}
+            style={{
+                cursor: 'pointer',
+                transition: 'all .1s ease-in-out'
+            }}
+        >
+            #
+        </li>)
+        for (var i = 0; i < 26; i++) {
+            let letter = String.fromCharCode(65 + i)
+            buttonList.push(<li
+                id='rolodex'
+                onClick={() => jumpToId(letter)}
+                style={{
+                    cursor: 'pointer',
+                    transition: 'all .2s ease-in-out'
+                }}
+            >
+                {letter}
+            </li>)
+        }
+        return buttonList
+    }
 
     return (
-        <Row>
-            <Col md={{ span: 16, offset: 4 }} xs={{ span: 24 }}>
-                {isLoading ? <h1 style={headerStyle}>Fetching Companies <Icon type='loading'/></h1>: <h1 style={headerStyle}>Browse Companies</h1>}
-                {allCompanies ?
-                    <List
-                        split={false}
-                        size="large"
-                        dataSource={allCompanies.map((company) => <CompanyListCard {...company} />)}
-                        renderItem={item => <List.Item style={{ padding: "0 !important" }}>{item}</List.Item>}
-                    /> :
-                    <h2>{!isLoading && "No Companies Found"} </h2>
-                }
-            </Col>
-        </Row>
+        <div>
+            <Row>
+                <Col md={1} style={{
+                    position: 'sticky',
+                    width: '60px',
+                    top: '10%',
+                    right: '0',
+                    zIndex: '100'
+                }}>
+                    {!isLoading &&
+                        <ul style={{
+                            listStyleType: 'none',
+                            fontWeight: '15px'
+                        }}>
+                            <li
+                                id='rolodex'
+                                style={{
+                                    cursor: 'default',
+                                    transition: 'all .2s ease-in-out'
+                                }}>
+                                <b>Jump To:</b>
+                            </li>
+                            {getSidebarButtons()}
+                        </ul>
+                    }
+                </Col>
+                <Col md={23}>
+                    <Row style={{ paddingTop: '5%' }} >
+                        {
+                            isLoading ?
+                                <h1 style={headerStyle}>Fetching Companies <Icon type='loading' /></h1> :
+                                <h1 style={headerStyle}>Browse Companies</h1>
+                        }
+                        <Search
+                            size='large'
+                            id='home-search'
+                            placeholder="Filter company by name"
+                            onSearch={onSearch}
+                            onChange={onInputChange}
+                            style={{ width: '50%', marginTop: '1em' }}
+                        />
+                    </Row>
+                    <Row style={{ paddingTop: '1%' }} >
+                        <Col md={{ span: 16, offset: 4 }} xs={{ span: 24 }}>
+                            {filteredCompanies ?
+                                <List
+                                    split={false}
+                                    size="large"
+                                    dataSource={filteredCompanies.map((company) => {
+                                        let char = company.name.toLowerCase().charAt(0)
+                                        return {
+                                            cname: isNaN(char) ? 'letter-' + char : 'letter-#',
+                                            domElement: <CompanyListCard {...company} />
+                                        }
+                                    })}
+                                    renderItem={item => <List.Item className={item.cname} style={{ padding: "0 !important" }}>{item.domElement}</List.Item>}
+                                /> :
+                                <h2>{!isLoading && "No Companies Found"} </h2>
+                            }
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+        </div>
     )
 }
 
