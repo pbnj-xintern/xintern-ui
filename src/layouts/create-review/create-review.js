68997c4725dfeb6a351fe6be8c7ccda838c6d38b
John Cen <john.cen397@gmail.com>
2019-11-26 03:20:54 -0500

    pulls in company data and location, onSubmit 403 forbidden error

--- src/layouts/create-review/create-review.js
+++ src/layouts/create-review/create-review.js
@@ -3,26 +3,48 @@ import React, { useEffect, useState } from 'react'
 import { Row, Col, Form, Icon, Input, Button, Tooltip, Select, Rate } from 'antd'
 import * as styles from './create-review.emotion' 
 import axios from 'axios'
-import { useLocation } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { useLocation, useHistory } from 'react-router-dom'
 
+const getAllCompanies = async () => {
+    let response = await axios.get('/company/all')
+    if (response.data.length == 0 || response.data.error) {
+        console.error("no locations found")
+        return []
+    }
+    return response.data
+}
 
-const hasErrors = (fieldsError) => {
-    return Object.keys(fieldsError).some(field => fieldsError[field]);
+const getCompanyLocations = async (companyName) => {
+    let qParams = { params: {
+        company_name: companyName
+    }}
+    let response = await axios.get('/company/locations', qParams)
+    if (response.data.length == 0 || response.data.error) {
+        console.error("no locations found")
+        return []
+    }
+    return response.data
 }
 
 const CreateReviewForm = (props) => {
-    const { getFieldDecorator } = props.form
+    // const { getFieldDecorator } = props.form
     let location = useLocation()
+    let history = useHistory()
     let companyName = location.pathname.split("/")[2]
+    // console.log('companyName:', companyName)
     const { Option } = Select
     const { TextArea } = Input
 
+    const [isLoading, setIsLoading] = useState(false)
+    const [companyLocations, setCompanyLocations] = useState([])
     const [payload, setPayload] = useState({
         culture: 0,
         mentorship: 0,
         impact: 0,
         interview: 0,
-        company_name: "",
+        company_name: (companyName === "create") ? "" : companyName,
+        location: "",
         salary: 0,
         content: "",
         position: "",
@@ -30,9 +52,15 @@ const CreateReviewForm = (props) => {
         payPeriod: ""
     })
 
-    // useEffect(() => {
-    //     props.form.validateFields()
-    // }, [])
+    const currencyOptions = ['CAD', 'USD', 'AUD', 'EUR']
+    const payPeriodOptions = ["MONTHLY", "HOURLY", "WEEKLY"]
+
+    useEffect(() => {
+        const fetchCompanyLocations = async () => { 
+            setCompanyLocations(await getCompanyLocations(companyName))
+        }
+        fetchCompanyLocations()
+    }, [])
 
     const formItemLayout = {
         labelCol: {
@@ -53,192 +81,111 @@ const CreateReviewForm = (props) => {
         }
     }
 
+    const onFieldChange = (e, key) => {
+        let newPayload = { ...payload }
+        if (key === "salary") {
+            newPayload[key] = parseInt(e.target.value)
+        } else {
+            newPayload[key] = (e.target) ? e.target.value : e
+        }
+        setPayload(newPayload)
+    }
+
+    //createReview endpoint
     const handleSubmit = async e => {
+        console.log('payload changed:\n', payload)
         e.preventDefault()
-        props.form.validateFieldsAndScroll((err, values) => {
-            if (!err) {
-                console.log('Received values of form: ', values);
-            }
-        })
-        setPayload({
-            culture: 0,
-            mentorship: 0,
-            impact: 0,
-            interview: 0,
-            company_name: "",
-            location: "",
-            salary: 0,
-            content: "",
-            position: "",
-            currency: "",
-            payPeriod: ""
-        })
         let response = await axios.post("/review", payload)
-        console.log('post response:\n', response)
+        console.log('create review res:\n', response.data)
+        if (response.status === 201) {
+            toast(response.data.message)
+            history.push(`/company/${companyName}/reviews`)
+        } else if (response.status !== 201 || !response) {
+            toast.error("Something went wrong. Could not post Review.")
+            return
+        }
     }
 
     return (
         <Row style={{ height: "100%", width: "100%", paddingTop: "7%", paddingBottom: "3%"}} >
             <Col xl={{ span: 16, offset: 4 }} css={styles.CreateReviewCol}>
-                <h1 style ={{ paddingTop: "2%", paddingBottom: "2%" }}>Create a Review</h1> 
+                <h1 style ={{ paddingTop: "5%", paddingBottom: "3%" }}>{(companyName) ? `${companyName}: Create a Review` : "Create a Review"}</h1> 
                 <Form {...formItemLayout} onSubmit={handleSubmit}>
                     <Row style={{ paddingBottom: "0.5%" }}>
                         <Col xl={{ span: 11, offset: 1 }}>
                             <Form.Item label="Company" labelCol={{ span: 6, offset: 1 }} labelAlign="left">
-                                {getFieldDecorator('company', {
-                                    rules: [
-                                    {
-                                        type: 'string',
-                                        message: 'No such Company found.',
-                                    },
-                                    {
-                                        required: true,
-                                        message: 'Please select a Company.',
-                                    },
-                                    ],
-                                })(<Select defaultValue={"hello"}/>)}
+                                {(companyName) ? 
+                                    <Select showSearch defaultValue={companyName} disabled>
+                                        <Option value={companyName}>{companyName}</Option>
+                                    </Select> : null}                                
                             </Form.Item>
                         </Col>
                         <Col xl={{ span: 8, offset: 0 }}>
                             <Form.Item label="Location" labelCol={{ span: 7, offset: 1 }} labelAlign="left">
-                                {getFieldDecorator('company-location', {
-                                    rules: [
-                                    {
-                                        type: 'string',
-                                        message: 'No such Location found.',
-                                    },
-                                    {
-                                        required: true,
-                                        message: 'Please select a Location.',
-                                    },
-                                    ],
-                                })(<Select />)}
+                                <Select showSearch placeholder="Select a Location" onChange={e => onFieldChange(e, 'location')}>
+                                    {companyLocations.map((location, i) => <Option key={i} value={location}>{location}</Option> )}                                
+                                </Select>                        
                             </Form.Item>
                         </Col>
                     </Row>
                     <Row>
                         <Col xl={{ span: 24, offset: 0 }} style={{ paddingLeft: "6%" }}>
                             <Form.Item label="Culture" labelCol={{ span: 4, offset: 0 }} labelAlign="left">
-                                {getFieldDecorator('culture-rating', {
-                                    rules: [
-                                        {
-                                            required: true,
-                                            message: 'Please select a Rating.',
-                                        }
-                                    ],
-                                    initialValue: 3.5
-                                })(<Col xl={{ span: 7 }}><Rate allowHalf /></Col>)}
+                                <Col xl={{ span: 7 }}>
+                                    <Rate allowHalf value={payload.culture} onChange={e => onFieldChange(e, 'culture')} />
+                                </Col>                               
                             </Form.Item>
                             <Form.Item label="Mentorship" labelCol={{ span: 4, offset: 0 }} labelAlign="left">
-                                {getFieldDecorator('mentorship-rating', {
-                                    rules: [
-                                        {
-                                            required: true,
-                                            message: 'Please select a Rating.',
-                                        }
-                                    ],
-                                    initialValue: 3.5
-                                })(<Col xl={{ span: 7 }}><Rate allowHalf /></Col>)}
+                                <Col xl={{ span: 7 }}>
+                                    <Rate allowHalf value={payload.mentorship} onChange={e => onFieldChange(e, 'mentorship')} />
+                                </Col>                                
                             </Form.Item>
                             <Form.Item label="Impact" labelCol={{ span: 4, offset: 0 }} labelAlign="left">
-                                {getFieldDecorator('impact-rating', {
-                                    rules: [
-                                        {
-                                            required: true,
-                                            message: 'Please select a Rating.',
-                                        }
-                                    ],
-                                    initialValue: 3.5
-                                })(<Col xl={{ span: 7 }}><Rate allowHalf /></Col>)}
+                                <Col xl={{ span: 7 }}>
+                                    <Rate allowHalf value={payload.impact} onChange={e => onFieldChange(e, 'impact')} />
+                                </Col>                                
                             </Form.Item>
                             <Form.Item label="Interview" labelCol={{ span: 4, offset: 0 }} labelAlign="left">
-                                {getFieldDecorator('interview-rating', {
-                                    rules: [
-                                        {
-                                            required: true,
-                                            message: 'Please select a Rating.',
-                                        }
-                                    ],
-                                    initialValue: 3.5
-                                })(<Col xl={{ span: 7 }}><Rate allowHalf /></Col>)}
+                                <Col xl={{ span: 7 }}>
+                                    <Rate allowHalf value={payload.interview} onChange={e => onFieldChange(e, 'interview')} />
+                                </Col>                            
                             </Form.Item>
                         </Col>
                     </Row>
                     <Row style={{ paddingBottom: "1%" }}>
                         <Col xl={{ span: 24, offset: 0 }}>
                             <Form.Item label="Position" labelCol={{ span: 3, offset: 1 }} labelAlign="left" style={{ paddingLeft: "2%" }}>
-                                {getFieldDecorator('position', {
-                                    rules: [
-                                        {
-                                            required: true,
-                                            message: 'Please enter a job position.',
-                                        }
-                                    ]
-                                })(<Input />)}
+                                <Input placeholder="Software Developer" value={payload.position} onChange={e => { onFieldChange(e, 'position') } } />                                
                             </Form.Item>
                         </Col>
                     </Row>
                     <Row style={{ paddingBottom: "1%" }}>
                         <Col xl={{ span: 12, offset: 0 }} style={{ paddingLeft: "2.5%" }}>
-                            <Form.Item label="Salary" labelCol={{ span: 6, offset: 0 }} labelAlign="left" style={{ paddingLeft: "10%" }}>
-                                {getFieldDecorator('salary', {
-                                    rules: [
-                                        {
-                                            type: 'number',
-                                            message: 'Please enter a number.',
-                                        }                                 
-                                    ]
-                                })(<Input />)}
+                            <Form.Item label="Salary ($)" labelCol={{ span: 7, offset: 0 }} labelAlign="left" style={{ paddingLeft: "7.5%" }}>
+                                <Input type="number" onChange={e => onFieldChange(e, 'salary')} />                               
                             </Form.Item>
                         </Col>
                         <Col xl={{ span: 6, offset: 0 }}>
                             <Form.Item label="Currency" labelCol={{ span: 8, offset: 0 }} labelAlign="left">
-                                {getFieldDecorator('currency', {
-                                    rules: [
-                                    {
-                                        type: 'string',
-                                        message: 'No such Location found.',
-                                    },                                    
-                                    ],
-                                })(<Select />)}
+                                <Select placeholder="Select a Currency" onChange={e => onFieldChange(e, 'currency')}>
+                                    {currencyOptions.map((currency, i) => <Option key={i} value={currency}>{currency}</Option> )}
+                                </Select>                                
                             </Form.Item>
                         </Col>
-                        {/* <Col xl={{ span: 8, offset: 1 }}>
-                            <Form.Item label="Pay Period" labelCol={{ span: 7, offset: 0 }} labelAlign="left">
-                                {getFieldDecorator('pay-period', {
-                                    rules: [
-                                    {
-                                        type: 'string',
-                                        message: 'No such Location found.',
-                                    },                                
-                                    ],
-                                })(<Select />)}
-                            </Form.Item>
-                        </Col> */}
                     </Row>
-                    <Col xl={{ span: 10, offset: 1 }} style={{ paddingLeft: "3%", paddingBottom: "1%" }}>
-                        <Form.Item label="Pay Period" labelCol={{ span: 7, offset: 0 }} labelAlign="left">
-                            {getFieldDecorator('pay-period', {
-                                rules: [
-                                {
-                                    type: 'string',
-                                    message: 'No such Location found.',
-                                },                                
-                                ],
-                            })(<Select />)}
-                        </Form.Item>
-                    </Col>
+                    <Row>
+                        <Col xl={{ span: 10, offset: 1 }} style={{ paddingLeft: "1.8%", paddingBottom: "1%" }}>
+                            <Form.Item label="Pay Period" labelCol={{ span: 7, offset: 0 }} labelAlign="left">
+                                <Select placeholder="Select a Pay Period" onChange={e => onFieldChange(e, 'payPeriod')}>
+                                    {payPeriodOptions.map((pp, i) => <Option key={i} value={pp}>{pp}</Option> )}
+                                </Select>                               
+                            </Form.Item>
+                        </Col>
+                    </Row>
                     <Row>
                         <Col xl={{ span: 23, offset: 1 }}>
-                            <Form.Item label="Content" labelCol={{ span: 3, offset: 0 }} labelAlign="left" style={{ paddingLeft: "2%" }}>
-                                {getFieldDecorator('content', {
-                                    rules: [
-                                        {
-                                            required: true,
-                                            message: "Please include a review."
-                                        }                             
-                                    ],
-                                })(<TextArea placeholder="Write something nice! (Or not)" autoSize={{ minRows: 8, maxRows: 24 }} />)}
+                            <Form.Item label="Review" labelCol={{ span: 3, offset: 0 }} labelAlign="left" style={{ paddingLeft: "2%" }}>
+                                <TextArea value={payload.content} onChange={e => onFieldChange(e, 'content')} placeholder="Write something nice! (Or not)" autoSize={{ minRows: 8, maxRows: 24 }} />                                
                             </Form.Item>
                         </Col>
                     </Row>            
