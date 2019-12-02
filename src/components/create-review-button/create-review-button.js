3dede4436129ed48fc4ab12d665b910d78109c7d
John Cen <john.cen397@gmail.com>
2019-12-01 19:21:10 -0500

    Disables create-review-button if unauthed

--- src/components/create-review-button/create-review-button.js
+++ src/components/create-review-button/create-review-button.js
@@ -3,9 +3,11 @@ import React from 'react'
 import { Button, Icon, Tooltip } from 'antd'
 import * as styles from './create-review-button.emotion'
 import { useLocation, useHistory } from 'react-router-dom'
+import { useAuthState } from '../../state/auth-state'
 
 const CreateReviewButton = () => {
     const location = useLocation()
+    const [authState, changeAuthState] = useAuthState();
     let history = useHistory()
     let path = location.pathname
     console.log('current path:', path)
@@ -14,10 +16,12 @@ const CreateReviewButton = () => {
         history.push("/review/create")
     }
 
+    const toolTipMessage = authState.isAuth ? "Create a Review!" : "Login to Create a Review!"
+
     return (
         (path.includes("/review/create") || path.includes("login") || path.includes("signup")) ? null : 
-        <Tooltip trigger="hover" title="Create a Review!">
-            <Button shape="circle" size="large" css={styles.ButtonStyles} onClick={handleOnClick}>
+        <Tooltip trigger="hover" title={toolTipMessage}>
+            <Button shape="circle" size="large" css={styles.ButtonStyles} onClick={(!authState.isAuth) ? null : handleOnClick}>
                 <Icon type="plus" css={styles.IconStyles} />
             </Button>
         </Tooltip>
